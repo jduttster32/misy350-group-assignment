ff56a3d3f35529d36015b82beb9e97b18bb53f2c
James Dutton <Jdutt@udel.edu>
2017-12-01 10:36:44 -0500

    added players to the player database and modified home page

--- manage.py
+++ manage.py
@@ -7,6 +7,11 @@ manager = Manager(app)
 def deploy():
     db.drop_all()
     db.create_all()
+    player1 = Player(name='James Dutton', height='6\'7\"', weight='219', position='PF')
+    player2 = Player(name='Jack Jacobs', height='6\'1\"', weight='180', position='SG')
+    db.session.add(player1)
+    db.session.add(player2)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
